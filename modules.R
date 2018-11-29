32b40565ab885bd1b132ffeae0054e9353b297e3
Sarah Wright <s.wright.618@gmail.com>
2018-11-29 13:44:28 -0800

    Added skeleton for file import server function and fleshed out file import UI function

--- modules.R
+++ modules.R
@@ -5,13 +5,52 @@ library(tidyverse)
 library(pool)
 library(DT)
 
+# Functions
+readFiles <- function (file.paths, file.names, search.string = "*", col.types = NULL) {
+  # Given a list of .csv file paths and file names, reads data into a data frame from files whose name matches the search string.
+  #
+  # Args:
+  #   file.paths: A list of paths to the files to be read
+  #   file.names: A list of the original filenames (if using R Shiny file input, this is not the same as file.paths!)
+  #   search.string: Regular expression specifying which files in file.names should be read
+  #   col.types: Column specification
+  # Returns:
+  #   A dataframe containing the data read from the input files. Note that duplicate rows will be removed.
+  
+  file.paths <- file.paths[grepl(search.string, file.names)]
+  
+  if (length(file.paths > 0)) {
+    data.in <- bind_rows(lapply(file.paths, read_csv, col_types = col.types))
+    data.in <- unique(data.in)  # Get rid of any duplicate rows of data
+  } else {
+    data.in <- data.frame()
+  }
+  
+  return(data.in)
+}
+
 # CSV file import module UI
 # TODO: read from JSON
-fileImportInput <- function(id, table.spec) {
-  # File import module that reads csv data files into dataframes in a reactiveValues object.
+fileImportInput <- function(id) {
+  # File import UI module.
   #
   # Args:
   #   id: The namespace for the module
+
+  ns <- NS(id)
+  
+  fileInput("files.in", "Select data files to upload",
+            multiple = TRUE,
+            accept = ".csv")
+  
+}
+
+# CSV file import module server function
+fileImport <- function(input, output, session, table.spec) {
+  # File import module that reads csv data files into dataframes in a reactiveValues object.
+  #
+  # Args:
+  #   input, output, session: required parameters for Shiny server function
   #   table.spec: A list of table types (e.g. CalibrationDO, WaterQualityActivity), each a list containing the following:
   #                 search.string: Regular expression specifying which files in file.names correspond to that table type
   #                 col.types: Column specification
@@ -22,3 +61,11 @@ fileImportInput <- function(id, table.spec) {
   
   
 }
+
+
+
+
+
+
+
+
