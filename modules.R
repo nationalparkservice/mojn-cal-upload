912c11fa7bb384c88d538de46993da34ec30a861
Sarah Wright <s.wright.618@gmail.com>
2018-11-29 14:52:52 -0800

    Start from original SpCond import code

--- modules.R
+++ modules.R
@@ -59,6 +59,33 @@ fileImport <- function(input, output, session, table.spec) {
   # Returns:
   #   A reactiveValues object containing the data read from the input files. Note that duplicate rows will be removed.
   
+  # Initialize reactiveValues object
+  data.imports <- reactiveValues()
+  
+  # When new files are uploaded, populate data.imports
+  observeEvent(input$files.in, {
+    data.in <- readFiles(input$files.in$datapath,
+                         input$files.in$name,
+                         "*_CalibrationSpCond.csv",
+                         col.types = cols(CalibrationDate = col_character(),
+                                          CalibrationTime = col_time(),
+                                          StandardValue_microS_per_cm = col_double(),
+                                          PreCalibrationReading_microS_per_cm = col_double(),
+                                          PostCalibrationReading_microS_per_cm = col_double(),
+                                          SpCondInstrumentGUID = col_character(),
+                                          Notes = col_character(),
+                                          GUID = col_character(),
+                                          DateCreated = col_character()))
+    if (nrow(data.in > 0)) {
+      data.in <- data.in %>%
+        mutate(CalibrationTime = format(CalibrationTime, "%H:%M:%S"),
+               CalibrationDate = as.Date(CalibrationDate, format = "%m/%d/%Y")) %>%  # Format dates and times so they display properly
+        left_join(db.ref.wqinstr, by = c("SpCondInstrumentGUID" = "GUID"), copy = TRUE) %>%  # Join to WQ instrument table
+        select(-Summary, -Model, -Manufacturer, -NPSPropertyTag, -IsActive, -SpCondInstrumentGUID, -Label) %>%  # Get rid of unnecessary columns
+        rename(SpCondInstrumentID = ID)
+    }
+    SpCond.uploads(data.in)
+  })
   
 }
 
