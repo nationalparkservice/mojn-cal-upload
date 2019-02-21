788237a3853ee23b1741b34a664038f155b65271
Sarah Wright <s.wright.618@gmail.com>
2019-02-21 10:21:36 -0800

    Comment out code to be rewritten

--- app.R
+++ app.R
@@ -37,25 +37,25 @@ server <- function(input, output, session) {
     clean.data
   })
   
-  # For each table in the table specification, create a tab for viewing and editing data. Create a reactiveValues object from the reviewed and edited data returned by the dataViewAndEdit module.
-  final.data <- lapply(table.spec, function(table){
-    force(table)  # force evaluation of the table argument so that the call to the dataViewAndEdit module gets the correct table information
-    appendTab("view.edit.tabs", tabPanel(table$display.name, dataViewAndEditUI(table$table.name), dataUploadUI(paste0(table$table.name, ".upload")), select = TRUE))
-    reactive(callModule(dataViewAndEdit, id = table$table.name, data = clean.data()[[table$table.name]], col.spec = table$col.spec))
-  })
-  
-  # Call the data upload module
-  # TODO: Do this with a for loop
-  observe({
-    final.data$CalibrationSpCond
-    final.data$CalibrationDO
-    final.data$CalibrationpH
-    isolate({
-      callModule(dataUpload, id = "CalibrationSpCond.upload", data = final.data$CalibrationSpCond(), table.spec = table.spec$CalibrationSpCond)
-      callModule(dataUpload, id = "CalibrationDO.upload", data = final.data$CalibrationDO(), table.spec = table.spec$CalibrationDO)
-      callModule(dataUpload, id = "CalibrationpH.upload", data = final.data$CalibrationpH(), table.spec = table.spec$CalibrationpH)
-    })
-  })
+  # # For each table in the table specification, create a tab for viewing and editing data. Create a reactiveValues object from the reviewed and edited data returned by the dataViewAndEdit module.
+  # final.data <- lapply(table.spec, function(table){
+  #   force(table)  # force evaluation of the table argument so that the call to the dataViewAndEdit module gets the correct table information
+  #   appendTab("view.edit.tabs", tabPanel(table$display.name, dataViewAndEditUI(table$table.name), dataUploadUI(paste0(table$table.name, ".upload")), select = TRUE))
+  #   reactive(callModule(dataViewAndEdit, id = table$table.name, data = clean.data()[[table$table.name]], col.spec = table$col.spec))
+  # })
+  # 
+  # # Call the data upload module
+  # # TODO: Do this with a for loop
+  # observe({
+  #   final.data$CalibrationSpCond
+  #   final.data$CalibrationDO
+  #   final.data$CalibrationpH
+  #   isolate({
+  #     callModule(dataUpload, id = "CalibrationSpCond.upload", data = final.data$CalibrationSpCond(), table.spec = table.spec$CalibrationSpCond)
+  #     callModule(dataUpload, id = "CalibrationDO.upload", data = final.data$CalibrationDO(), table.spec = table.spec$CalibrationDO)
+  #     callModule(dataUpload, id = "CalibrationpH.upload", data = final.data$CalibrationpH(), table.spec = table.spec$CalibrationpH)
+  #   })
+  # })
 }
 
 # Run the application 
