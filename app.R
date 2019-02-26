c8be95b5a4a38d8c91a67bbd9c898a2bf44293af
Sarah Wright <s.wright.618@gmail.com>
2019-02-25 16:43:26 -0800

    Replace html template with Shiny UI

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyjs)
 library(odbc)
 library(dbplyr)
 library(tidyverse)
@@ -11,8 +12,84 @@ source("modules.R")
 source("tableSpec.R")
 
 # Define UI for application that imports calibration data from .csv and uploads to database
-ui <- htmlTemplate("html-templates/calibration-main.html")
-  
+ui <- tagList(
+  useShinyjs(),
+  navbarPage("Water Quality Calibration Data",
+             tabPanel("Upload",
+                      fluidPage(
+                        fluidRow(
+                          column(10, offset = 1,
+                                 # Data import card
+                                 tags$div(id = "import.card", class = "panel panel-default ",
+                                          tags$div(class = "panel-body",
+                                                   fluidRow(
+                                                     column(12, align = "center",
+                                                            fileImportInput("import.data")
+                                                     )
+                                                   )
+                                          )
+                                 ),
+                                 # Data review and edit card
+                                 hidden(
+                                   tags$div(id = "review.card", class = "panel panel-default ",
+                                            tags$div(class = "panel-body",
+                                                     fluidRow(
+                                                       column(10, offset = 1,
+                                                              tabsetPanel(type = "pills",
+                                                                          tabPanel("SpCond",
+                                                                                   dataViewAndEditUI("CalibrationSpCond")
+                                                                          ),
+                                                                          tabPanel("DO",
+                                                                                   dataViewAndEditUI("CalibrationDO")
+                                                                          ),
+                                                                          tabPanel("pH",
+                                                                                   dataViewAndEditUI("CalibrationpH")
+                                                                          )
+                                                              )
+                                                       )
+                                                     )
+                                            )
+                                   )
+                                 ),
+                                 # Data upload card
+                                 hidden(
+                                   tags$div(id = "upload.card", class = "panel panel-default ",
+                                            tags$div(class = "panel-body",
+                                                     fluidRow(
+                                                       column(12, align = "center",
+                                                              dataUploadUI("data.upload")
+                                                       )
+                                                     )
+                                            )
+                                   )
+                                 ),
+                                 # Pager buttons for toggling between cards
+                                 tags$nav(
+                                   tags$ul(class = "pager",
+                                           tags$li(id = "next.review", class = "next",
+                                                   tags$a(id = "btn.next.review", href = "#", "Review Data")),
+                                           hidden(
+                                             tags$li(id = "next.upload", class = "next",
+                                                     tags$a(id = "btn.next.upload", href = "#", "Upload Data"))),
+                                           hidden(
+                                             tags$li(id = "back.import", class = "previous",
+                                                     tags$a(id = "btn.back.import", href = "#", "Back to Import"))),
+                                           hidden(
+                                             tags$li(id = "back.review", class = "previous",
+                                                     tags$a(id = "btn.back.review", href = "#", "Back to Data Review")))
+                                   )
+                                 )
+                          )
+                          
+                        )
+                        
+                      )
+             ),
+             tabPanel("View"),
+             tabPanel("QA/QC")
+  )
+)
+
 # Define server logic
 server <- function(input, output, session) {
   
@@ -56,6 +133,43 @@ server <- function(input, output, session) {
   #     callModule(dataUpload, id = "CalibrationpH.upload", data = final.data$CalibrationpH(), table.spec = table.spec$CalibrationpH)
   #   })
   # })
+  # Handle events
+  
+  # Advance from import screen to review screen
+  onclick("btn.next.review", {
+    hide("next.review")
+    hide("import.card")
+    show("review.card")
+    show("next.upload")
+    show("back.import")
+  })
+  
+  # Advance from review screen to upload screen
+  onclick("btn.next.upload", {
+    hide("next.upload")
+    hide("back.import")
+    hide("review.card")
+    show("upload.card")
+    show("back.review")
+  })
+  
+  # Go back to review screen from upload screen
+  onclick("btn.back.review", {
+    hide("upload.card")
+    hide("back.review")
+    show("next.upload")
+    show("back.import")
+    show("review.card")
+  })
+  
+  # Go back to import screen from review screen
+  onclick("btn.back.import", {
+    hide("review.card")
+    hide("next.upload")
+    hide("back.import")
+    show("next.review")
+    show("import.card")
+  })
 }
 
 # Run the application 
